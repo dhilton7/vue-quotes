c8e8d7641dd26518185bb7dcd506954bf67fc2be
Dan Hilton <dhilton@missiondata.com>
2017-02-01 14:10:17 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+    	<AppHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"/>
+    	<NewQuote @quoteAdded="addQuote"/>
+    	<QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"/>
+    	<div class="row">
+    		<div class="col-sm-12 text-center">
+    			<div class="alert alert-info">
+    				Info: Click on a quote to delete it.
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
+	import AppHeader from './components/Header.vue';
+
+    export default {
+        data: function() {
+        	return {
+        		quotes: ['Sample quote to see something.'],
+        		maxQuotes: 10	
+        	}
+        },
+        components: {
+        	QuoteGrid,
+        	NewQuote,
+        	AppHeader
+        },
+        methods: {
+        	addQuote(quote) {
+        		if (this.quotes.length >= this.maxQuotes) {
+        			return alert('please delete quotes first');
+        		}
+        		this.quotes.push(quote)
+        	},
+        	deleteQuote(index) {
+        		this.quotes.splice(index, 1);
+        	}
+        }
+    }
+</script>
+
+<style>
+</style>
